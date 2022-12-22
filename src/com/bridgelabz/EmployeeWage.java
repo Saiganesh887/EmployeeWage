1b91cf65f47b20538f45d2e5919cfc23ca08857b
Your Name <you@example.com>
2022-12-22 16:48:00 +0530

    Employee Daily Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,6 +1,12 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static int wage_per_hour = 20;
+    static int full_day_hrs = 8;
+    static void wagePerFullDay(){
+        int daily_wage = wage_per_hour * full_day_hrs;
+        System.out.println(daily_wage);
+    }
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int x = (int)(Math.round((Math.random())*10))%2;
@@ -9,5 +15,7 @@ public class EmployeeWage {
         } else{
             System.out.println("Employee is absent");
         }
+        System.out.println("Daily Wage is: ");
+        wagePerFullDay();
     }
 }
