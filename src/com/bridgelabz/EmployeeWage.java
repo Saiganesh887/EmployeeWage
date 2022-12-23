8ec205a48648459921f444ddda04478e2cb0c2e3
Saiganesh Koppula <64762244+Saiganesh887@users.noreply.github.com>
2022-12-23 13:51:03 +0530

    Update EmployeeWage.java

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,20 +3,15 @@ package com.bridgelabz;
 public class EmployeeWage {
     static int WAGE_DAY_HOUR = 20;
     static int FULL_DAY_HRS = 8;
-         
-    static void wagePerFullDay(){
-           int daily_wage = WAGE_DAY_HOUR * FULL_DAY_HRS;
-           System.out.println(daily_wage);
-    }
     public static void main(String[] args){
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        System.out.println("Welcome to Employee Wage Computation");
         int x = (int)(Math.round((Math.random())*10))%2;
+        int daily_wage = WAGE_DAY_HOUR * FULL_DAY_HRS;
         if(x == 1){
             System.out.println("Employee is present");
+            System.out.println("Daily Wage is: "+ daily_wage);
         } else{
             System.out.println("Employee is absent");
         }
-        System.out.println("Daily Wage is: ");
-        wagePerFullDay();
     }
 }
